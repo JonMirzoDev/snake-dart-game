c4f7a4bd6b574a3925844eef45b9c7dc9981f5ac
JonMirzoDev <islomjon.swe@gmail.com>
2024-04-11 11:21:43 +0500

    drawCell and clear function created

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,31 @@
-import 'package:web/helpers.dart';
+import 'dart:html';
+import 'dart:math';
+
+const int CELL_SIZE = 10;
+
+CanvasElement? canvas;
+CanvasRenderingContext2D? ctx;
 
 void main() {
-  final now = DateTime.now();
-  final element = document.querySelector('#output') as HTMLDivElement;
-  element.text = 'The time is ${now.hour}:${now.minute}'
-      ' and your Dart web app is running!';
+  canvas = querySelector('#canvas') as CanvasElement;
+  ctx = canvas?.getContext('2d') as CanvasRenderingContext2D?;
+}
+
+void drawCell(Point coords, String color) {
+  ctx
+    ?..fillStyle = color
+    ..strokeStyle = "white";
+
+  final x = (coords.x * CELL_SIZE).toInt();
+  final y = (coords.y * CELL_SIZE).toInt();
+
+  ctx
+    ?..fillRect(x, y, CELL_SIZE, CELL_SIZE)
+    ..strokeRect(x, y, CELL_SIZE, CELL_SIZE);
+}
+
+void clear() {
+  ctx
+    ?..fillStyle = "white"
+    ..fillRect(0, 0, canvas?.width ?? 0, canvas?.height ?? 0);
 }
